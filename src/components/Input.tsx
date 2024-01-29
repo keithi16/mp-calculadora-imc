1cdbd09df10cf55851f71e7ff0e43ab8b5113a34
Keithi Shimogava <contatokeithizin@gmail.com>
2024-01-29 11:37:09 -0300

    Final commit

---
+++ src/components/Input.tsx
@@ -0,0 +1,19 @@
+import { InputHTMLAttributes } from "react";
+import { cn } from "../lib/utils";
+
+function Input({
+  className,
+  ...props
+}: InputHTMLAttributes<HTMLInputElement> & { classname?: string }) {
+  return (
+    <input
+      className={cn(
+        `block w-full border border-rose-400 rounded p-3`,
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
+export default Input;
